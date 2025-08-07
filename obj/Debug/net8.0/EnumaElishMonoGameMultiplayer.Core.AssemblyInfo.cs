5e6e7d8ff1f78c9e5074c989defb8cc48bfec6d6
Memphis <memphisyomael@gmail.com>
2025-08-07 09:21:21 -0400

    commit

--- obj/Debug/net8.0/EnumaElishMonoGameMultiplayer.Core.AssemblyInfo.cs
+++ obj/Debug/net8.0/EnumaElishMonoGameMultiplayer.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnumaElishMonoGameMultiplayer.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+818615a6a93e468939d99bfaed5851f1203d6b57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed97a1f329007004b2391b5cf4fa8f2becd41bf9")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnumaElishMonoGameMultiplayer.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnumaElishMonoGameMultiplayer.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
